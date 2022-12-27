a388affe21f3f06bbffea54e76ed61782232a624
dassshic <goryaeva.daria@gmail.com>
2022-12-27 10:24:37 +0300

    переделала дз

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-
-        int[] array = new int[30];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = ((int)(Math.random() * 100_001) + 100_000);
-        }
+        int[] array = generateRandomArray(30);
         System.out.println("task1");
         int sum = 0;
         for (int item : array) {
@@ -20,21 +18,27 @@ public class Main {
             if (k > maxSum)
                 maxSum = k;
         }
-        System.out.println("Минимальная сумма трат за день составила " + minSum + " рублей. Максимальная сумма трат за день составила " + maxSum +" рублей");
+        System.out.println("Минимальная сумма трат за день составила " + minSum + " рублей. Максимальная сумма трат за день составила " + maxSum + " рублей");
 
         System.out.println("task3");
         for (int j : array) {
             sum += j;
         }
-        double average = (double)sum / array.length;
-        System.out.println("Средняя сумма трат за месяц составила "+ average + " рублей");
+        double average = (double) sum / array.length;
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
 
         System.out.println("task4");
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--)
-        {
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
         }
     }
-}
-//привет
+        private static int[] generateRandomArray(int size) {
+            int[] array = new int[size];
+            Random random = new Random();
+            for (int i = 0; i < array.length; i++) {
+                array[i] = random.nextInt(100_000, 200_000);
+            }
+            return array;
+        }
+    }
